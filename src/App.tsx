eb461868f3655e1b2ec773bdf7fb3fac00a0f8a2
Lisa Nguyen <ngocanhthu.work@gmail.com>
2025-07-23 11:42:38 -0400

    Cleaning code

--- src/App.tsx
+++ src/App.tsx
@@ -60,15 +60,6 @@ function App() {
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-900 via-purple-900 to-gray-900 flex items-center justify-center p-4">
-      {/* Hidden video element for hand detection */}
-      {/* <video
-        ref={videoRef}
-        className="hidden"
-        autoPlay
-        muted
-        playsInline
-      /> */}
-
       {/* Start Menu */}
       {showStartMenu && (
         <StartMenu onStart={handleStartGame} />
